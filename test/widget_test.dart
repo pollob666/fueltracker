e9d7b39480275aa114ee1259b17b76fb1b48784b
Andalib Bin Haque <andalib.haque@dnet.org.bd>
2025-02-09 23:15:22 +0600

    Initial code by ChatGPT

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fueltracker/fuel_tracker_app.dart';
 
 import 'package:fueltracker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const FuelTrackerApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
