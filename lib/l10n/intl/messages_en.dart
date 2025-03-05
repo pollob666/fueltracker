3f8aa16d9e51e4db111de00c322612fa3e9169ad
Andalib Bin Haque <andalib.haque@dnet.org.bd>
2025-03-05 12:02:51 +0600

    test code 2

---
+++ lib/l10n/intl/messages_en.dart
@@ -0,0 +1,110 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "addFuelData": MessageLookupByLibrary.simpleMessage("Add Fuel Data"),
+    "allData": MessageLookupByLibrary.simpleMessage("All Data"),
+    "appTitle": MessageLookupByLibrary.simpleMessage(
+      "Fuel Consumption Tracker",
+    ),
+    "csvFileEmptyOrIncorrectlyFormatted": MessageLookupByLibrary.simpleMessage(
+      "CSV file is empty or incorrectly formatted.",
+    ),
+    "dashboard": MessageLookupByLibrary.simpleMessage("Dashboard"),
+    "dataExportedTo": MessageLookupByLibrary.simpleMessage("Data exported to "),
+    "dataImportedSuccessfully": MessageLookupByLibrary.simpleMessage(
+      "Data Imported Successfully!",
+    ),
+    "dateAndTime": MessageLookupByLibrary.simpleMessage("Date & Time"),
+    "dropbox": MessageLookupByLibrary.simpleMessage("Dropbox"),
+    "enterFuelPriceRate": MessageLookupByLibrary.simpleMessage(
+      "Enter fuel price rate",
+    ),
+    "enterMaximumCapacity": MessageLookupByLibrary.simpleMessage(
+      "Enter maximum capacity",
+    ),
+    "enterOdometerReading": MessageLookupByLibrary.simpleMessage(
+      "Enter odometer reading",
+    ),
+    "enterPaidAmount": MessageLookupByLibrary.simpleMessage(
+      "Enter paid amount",
+    ),
+    "enterTotalVolume": MessageLookupByLibrary.simpleMessage(
+      "Enter total volume",
+    ),
+    "errorImportingData": MessageLookupByLibrary.simpleMessage(
+      "Error importing data: ",
+    ),
+    "errorReadingCSVFile": MessageLookupByLibrary.simpleMessage(
+      "Error reading CSV file: ",
+    ),
+    "export": MessageLookupByLibrary.simpleMessage("Export"),
+    "exportToCSV": MessageLookupByLibrary.simpleMessage("Export to CSV"),
+    "failedToExport": MessageLookupByLibrary.simpleMessage(
+      "Failed to export: ",
+    ),
+    "fileStorageOption": MessageLookupByLibrary.simpleMessage(
+      "File Storage Option",
+    ),
+    "folderPath": MessageLookupByLibrary.simpleMessage("Folder Path"),
+    "folderPathNotSetInSettings": MessageLookupByLibrary.simpleMessage(
+      "Folder path not set in settings.",
+    ),
+    "fuelPriceRate": MessageLookupByLibrary.simpleMessage(
+      "Fuel Price Rate (BDT)",
+    ),
+    "fuelType": MessageLookupByLibrary.simpleMessage("Fuel Type"),
+    "googleDrive": MessageLookupByLibrary.simpleMessage("Google Drive"),
+    "import": MessageLookupByLibrary.simpleMessage("Import"),
+    "importData": MessageLookupByLibrary.simpleMessage("Import Data"),
+    "lastRefuelVolume": MessageLookupByLibrary.simpleMessage(
+      "Last Refuel Volume",
+    ),
+    "lastTimeMileage": MessageLookupByLibrary.simpleMessage(
+      "Last Time Mileage",
+    ),
+    "local": MessageLookupByLibrary.simpleMessage("Local"),
+    "maximumTankCapacity": MessageLookupByLibrary.simpleMessage(
+      "Maximum Tank Capacity (litres)",
+    ),
+    "noDataPreviewAvailable": MessageLookupByLibrary.simpleMessage(
+      "No Data Preview Available",
+    ),
+    "odometerReading": MessageLookupByLibrary.simpleMessage(
+      "Odometer Reading (km)",
+    ),
+    "paidAmount": MessageLookupByLibrary.simpleMessage("Paid Amount (BDT)"),
+    "runningAverageMileage": MessageLookupByLibrary.simpleMessage(
+      "Running Average Mileage",
+    ),
+    "save": MessageLookupByLibrary.simpleMessage("Save"),
+    "saveSettings": MessageLookupByLibrary.simpleMessage("Save Settings"),
+    "selectCSVFile": MessageLookupByLibrary.simpleMessage("Select CSV File"),
+    "selectedFile": MessageLookupByLibrary.simpleMessage("Selected File"),
+    "settings": MessageLookupByLibrary.simpleMessage("Settings"),
+    "settingsSaved": MessageLookupByLibrary.simpleMessage("Settings saved."),
+    "totalVolume": MessageLookupByLibrary.simpleMessage(
+      "Total Volume (litres)",
+    ),
+  };
+}
