90c7799992aff55180e06b599160f80230c4cfa7
Andalib Bin Haque <pollob666@gmail.com>
2025-10-23 14:23:58 +0600

    feat: Update dependencies
    Updates various package dependencies to their latest versions.
    
    This includes major version bumps for several key packages such as `google_sign_in`, `googleapis`, `fl_chart`, and `permission_handler`.
    
    Also registers the `file_picker` plugin for the macOS platform.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
@@ -12,6 +13,7 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
