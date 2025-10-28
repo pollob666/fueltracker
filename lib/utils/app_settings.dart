4914aa750fadfb8da9904597faed43639fe34920
Andalib Bin Haque <pollob666@gmail.com>
2025-10-28 11:36:27 +0600

    feat: Add update check functionality
    This commit introduces a feature to check for new application updates from GitHub releases.
    
    - Adds a `VersionService` to compare the current app version with the latest release tag on GitHub.
    - On the dashboard, if an update is available, a `SnackBar` is shown with a link to the latest release page.
    - Adds `http`, `package_info_plus`, and `url_launcher` as new dependencies.
    - Registers the `url_launcher` and `package_info_plus` plugins for macOS, Windows, and Linux platforms.
    - Includes a minor cleanup by removing an unused import in `app_settings.dart`.

--- lib/utils/app_settings.dart
+++ lib/utils/app_settings.dart
@@ -3,7 +3,6 @@
 
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppSettings {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,18 @@ import Foundation
 
 import dynamic_color
 import file_picker
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
